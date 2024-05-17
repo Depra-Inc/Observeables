b51df6644d751fabf4ab28222b093a7cd28b47bb
godzzz <g0dzZz1lla@yandex.ru>
2024-05-18 02:35:29 +0400

    Added AsTask extension

--- Expectation/Base/ExpectantExtensions.cs
+++ Expectation/Base/ExpectantExtensions.cs
@@ -2,6 +2,7 @@
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
 using System;
+using System.Threading.Tasks;
 
 namespace Depra.Expectation
 {
@@ -10,5 +11,18 @@ namespace Depra.Expectation
 		public static void Subscribe(this IExpectant self, Action callback) => self.Subscribe(callback, callback);
 
 		public static void Unsubscribe(this IExpectant self, Action callback) => self.Unsubscribe(callback);
+
+		/// <summary>
+		/// Extension method that converts an <see cref="IExpectant"/> into a <see cref="Task"/>.
+		/// </summary>
+		/// <param name="self">The <see cref="IExpectant"/> to convert.</param>
+		/// <returns>A Task that represents the completion of the <see cref="IExpectant"/>.</returns>
+		public static Task AsTask(this IExpectant self)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			self.Subscribe(() => tcs.SetResult(true));
+
+			return tcs.Task;
+		}
 	}
 }
